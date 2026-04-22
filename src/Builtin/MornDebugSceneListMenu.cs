b77605ba8101408f7da7ad9bf4dcc088e5e397fe
matsufriends <pikapikan0526@gmail.com>
2026-04-22 09:12:50 +0900

    fix: Switch ビルド時のシリアライズレイアウト警告を解消
    MornDebugSceneListMenu クラス全体が #if UNITY_EDITOR で囲まれていたため、
    非 Editor ビルドでクラス自体が存在せず "A scripted object ... has a different
    serialization layout when loading" の警告が出ていた。
    
    - クラス本体はランタイムにも残し、Editor 依存箇所だけ #if UNITY_EDITOR で分岐
    - _rootSceneFolder を Editor/ランタイムで同じシリアライズレイアウトにするため
      DefaultAsset → UnityEngine.Object に変更 (Editor 側で DefaultAsset として利用)
    - GetMenuItems は非 Editor で yield break
    
    version 1.0.4 に bump。

--- src/Builtin/MornDebugSceneListMenu.cs
+++ src/Builtin/MornDebugSceneListMenu.cs
@@ -1,14 +1,21 @@
-#if UNITY_EDITOR
 using System;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace MornLib
 {
     [CreateAssetMenu(fileName = nameof(MornDebugSceneListMenu), menuName = "Morn/Debug/" + nameof(MornDebugSceneListMenu))]
     public sealed class MornDebugSceneListMenu : MornDebugMenuBase
     {
+        // フィールドは Editor / ランタイム で同一レイアウトにして、ビルド時のシリアライズレイアウト差分警告を避ける。
+        // _rootSceneFolder は Editor では DefaultAsset をドロップする想定。型は UnityEngine.Object として保持する。
+        [SerializeField] private string _menuKey = "シーン一覧";
+        [SerializeField] private UnityEngine.Object _rootSceneFolder;
+
+#if UNITY_EDITOR
         private sealed class SceneAssetTree : MornDebugTreeBase<EditorBuildSettingsScene>
         {
             public SceneAssetTree(string prefix) : base(prefix)
@@ -26,13 +33,14 @@ namespace MornLib
             }
         }
 
-        [SerializeField] private string _menuKey = "シーン一覧";
-        [SerializeField] private DefaultAsset _rootSceneFolder;
         private SceneAssetTree _sceneAssetTree;
+#endif
 
         public override IEnumerable<(string key, Action action)> GetMenuItems()
         {
-            var prefix = _rootSceneFolder != null ? AssetDatabase.GetAssetPath(_rootSceneFolder) + "/" : "";
+#if UNITY_EDITOR
+            var folderPath = _rootSceneFolder != null ? AssetDatabase.GetAssetPath(_rootSceneFolder) : "";
+            var prefix = string.IsNullOrEmpty(folderPath) ? "" : folderPath + "/";
             _sceneAssetTree = new SceneAssetTree(prefix);
             foreach (var scene in EditorBuildSettings.scenes)
             {
@@ -46,7 +54,9 @@ namespace MornLib
                     _sceneAssetTree.OnGUI();
                 }
             });
+#else
+            yield break;
+#endif
         }
     }
 }
-#endif
